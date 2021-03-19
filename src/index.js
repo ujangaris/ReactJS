a0871e63b7f9bc7951e07da5cb44e0478e617026
ujangaris <ujangaja@gmail.com>
2021-03-19 21:31:33 +0700

    Coba React

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Kontak from './pages/Kontak';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Kontak />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/pages/Kontak.js
@@ -0,0 +1,10 @@
+function Kontak() {
+    return (
+        <div className="App">
+            <h1>Kontak Belajar React</h1>
+            <p>Jakarta</p>
+        </div>
+    );
+}
+
+export default Kontak;
