bd85f5743f3602123e107aecc6eb37b5d618a62b
ujangaris <ujangaja@gmail.com>
2021-03-20 20:28:18 +0700

    #3 Menambahkan Gambar

--- src/pages/Sejarah.js
+++ src/pages/Sejarah.js
@@ -3,6 +3,7 @@ function Sejarah() {
         <div className="App">
             <h1>Sejarah Belajar React</h1>
             <p>Isi Sejarah React</p>
+            <img src="images/logo.png" alt="logo"/>
         </div>
     );
 }
