cdac1a860d0344c89d43763f94d9307b44e9e471
ujangaris <ujangaja@gmail.com>
2021-03-21 20:49:52 +0700

    #6 Menambahkan bootstrap

--- src/pages/Tentang.js
+++ src/pages/Tentang.js
@@ -3,6 +3,7 @@ function Tentang() {
         <div className="App">
             <h1>Tentang Belajar React</h1>
             <p>Isi Tentang Belajar React</p>
+            <button type="button" class="btn btn-primary">Button</button>
         </div>
     );
 }
