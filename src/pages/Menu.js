8df656cc2579efa6f6e724915ffabcfca5af9168
ujangaris <ujangaja@gmail.com>
2021-04-26 20:56:38 +0700

    #11 Contoh reuse Komponen | menampilkan data es dengan table

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -14,14 +14,14 @@ function Menu() {
             },
             {
                 idmenu: 1,
-                idkategori: 2,
+                idkategori: 3,
                 menu: "Es Jeruk",
                 gambar: "esjeruk.jpg",
                 harga: 10000,
             },
             {
                 idmenu: 2,
-                idkategori: 3,
+                idkategori: 2,
                 menu: "Roti Bakar",
                 gambar: "rotibakar.jpg",
                 harga: 15000,
@@ -31,6 +31,7 @@ function Menu() {
     return (
         <div className="App">
             <Table menu={menus} titel={titel} />
+            <Table menu={menus.filter((data)=>(data.idkategori=== 3))} titel="Menu Es" />
         </div>
     );
 }
